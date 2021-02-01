cef26b1d63baa844ed6f8589a150b45dea74370a
He TianGang <allenhe77@gmail.com>
2021-02-01 11:09:07 +0300

    template feature, haven't fully completed yet

--- goserver/hello.go
+++ goserver/hello.go
@@ -9,15 +9,70 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+const doc = `
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+		
+		<table>
+			{{range .Keys}}
+
+			<tr>
+				<td>{{.}}</td>
+
+				
+			</tr>
+
+			
+			
+		{{end}}
+		</table>
+       
+    </body>
+</html>`
+
+type Pair struct {
+	Key   string
+	Value []string
+}
+
+type Keys struct {
+	Pairs []Pair
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprint(w, "Hi there I love %s", r.URL.Path[1:])
 	opsProcessed.Inc()
 	headers := r.Header
+	// w.Header().Add("Content Type", "text/html")
+	// templates := template.New("template")
+	// templates.New("doc").Parse(doc)
+	a := Keys{}
 
 	for index, element := range headers {
-		fmt.Fprint(w, index)
-		fmt.Fprintln(w, element)
+		// fmt.Fprint(w, reflect.TypeOf(index))
+		// fmt.Fprintln(w, reflect.TypeOf(element))
+
+		pair := Pair{}
+		pair.Key = index
+
+		for _, value := range element {
+			pair.Value = append(pair.Value, value)
+		}
+		// for c, d := range element {
+
+		// }
+		a.Pairs = append(a.Pairs, pair)
+
 	}
+
+	fmt.Println(a)
+
+	// templates.Lookup("doc").Execute(w, a)
+
 }
 
 // func recordMetric() {
@@ -39,8 +94,8 @@ var (
 func main() {
 	// recordMetric()
 	http.Handle("/metrics", promhttp.Handler())
+	// http.HandleFunc("/", handler)
 	http.HandleFunc("/", handler)
-
-	http.ListenAndServe(":3117", nil)
+	http.ListenAndServe(":8080", nil)
 
 }
